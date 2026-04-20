87799768591719aefca6bc58dd0fef262d67173c
fivet06py-droid <fivet06py@gmail.com>
2026-04-20 10:44:20 +0700

    Update BanVeUI.java

--- DeTaiJava/src/UI/BanVeUI.java
+++ DeTaiJava/src/UI/BanVeUI.java
@@ -2,15 +2,25 @@ package UI;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class BanVeUI extends JPanel {
     public BanVeUI() {
-        setLayout(new BorderLayout());
-        setBackground(Color.WHITE);
-
-        add(new JLabel("Màn hình Bán Vé"), BorderLayout.CENTER);
+       
+        JPanel pnMain = new JPanel();
+        pnMain.setLayout(new BorderLayout());
+        pnMain.setBackground(Color.YELLOW);
+        add(pnMain);
+        JPanel pnNorth = new JPanel();
+        pnNorth.setLayout(new BorderLayout());
+        JLabel lblTitleBanVe = new JLabel("Bán Vé");
+        lblTitleBanVe.setFont(new Font("Arial", Font.BOLD, 40));
+        lblTitleBanVe.setForeground(Color.WHITE);
+        pnNorth.add(lblTitleBanVe, BorderLayout.CENTER);
+        pnMain.add(pnNorth, BorderLayout.NORTH);
+        
     }
 }
